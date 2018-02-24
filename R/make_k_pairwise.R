9a3273eceac35b5ab5655dee8a9d9245229cd410
em <emjosephs@gmail.com>
2018-02-23 16:02:50 -0800

    Stuff

--- R/make_k_pairwise.R
+++ R/make_k_pairwise.R
@@ -1,8 +1,7 @@
 
-##makes a kinship matrix using pairwise complete data. input is a matrix where the rows are loci and the columns are individuals or populations and the values are the number of allele copies in each individual (ie 2x allele frequency).
+##makes a kinship matrix using pairwise complete data. input is a matrix where the rows are loci and the columns are individuals or populations and the values are the allele frequency.
 
-make_k_pairwise <- function(myMatrix){
-mygenos = as.matrix(myMatrix)/2 #divide to get frequencies
+make_k_pairwise <- function(mygenos){
 scaleFactor = 1/sqrt(rowMeans(mygenos, na.rm=T)*(1-rowMeans(mygenos, na.rm=T)))  #scale (rows are locus so divde each by sqrt or mean(1-mean))
 myGcent = scale(mygenos, scale=F)
 myGstand =  t(sapply(1:nrow(myGcent), function(x) {myGcent[x,]/scaleFactor[x]}))
