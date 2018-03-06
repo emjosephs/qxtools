e05cdae7aaee69453c3d1ce6019977d9929399b3
em <emjosephs@gmail.com>
2018-03-06 11:43:27 -0800

    Stuff

--- R/make_k_complete.R
+++ R/make_k_complete.R
@@ -1,7 +1,7 @@
 
 ##makes a kinship matrix using the cov function. input is a matrix where the rows are loci and the columns are individuals or populations and the values are the allele frequency.
 
-make_k_pairwise <- function(mygenos){
+make_k_complete <- function(mygenos){
 scaleFactor = 1/sqrt(rowMeans(mygenos, na.rm=T)*(1-rowMeans(mygenos, na.rm=T)))  #scale (rows are locus so divde each by sqrt or mean(1-mean))
 myGcent = scale(mygenos, scale=F)
 myGstand =  t(sapply(1:nrow(myGcent), function(x) {myGcent[x,]/scaleFactor[x]}))
