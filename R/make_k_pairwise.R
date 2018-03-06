96e7f94980d62efdbc401a1e0dd1670c21ab4d02
em <emjosephs@gmail.com>
2018-03-06 15:04:21 -0800

    stuff

--- R/make_k_pairwise.R
+++ R/make_k_pairwise.R
@@ -1,11 +1,15 @@
 
-##makes a kinship matrix using pairwise complete data. input is a matrix where the rows are loci and the columns are individuals or populations and the values are the allele frequency.
+##makes a kinship matrix using the cov function. input is a matrix where the rows are individuals/populations and the columns are loci and the values are the allele frequency.
 
-make_k_pairwise <- function(mygenos){
-scaleFactor = 1/sqrt(rowMeans(mygenos, na.rm=T)*(1-rowMeans(mygenos, na.rm=T)))  #scale (rows are locus so divde each by sqrt or mean(1-mean))
-myGcent = scale(mygenos, scale=F)
-myGstand =  t(sapply(1:nrow(myGcent), function(x) {myGcent[x,]/scaleFactor[x]}))
-myK = cov(myGstand, use="pairwise.complete.obs")
+make_k_complete <- function(myG){
+scaleFactor = 1/sqrt(colMeans(myG) * (1 - colMeans(myG)))
+myS = matrix(0, nrow = dim(myG)[2], ncol = dim(myG)[2])
+diag(myS) = scaleFactor
+myM = dim(myG)[1]
+myT = matrix(data = -1/myM, nrow = myM - 1, ncol = myM)
+diag(myT) = (myM - 1)/myM
+myGstand = myT %*% myG %*% myS
+myK = cov(t(myGstand), use="pairwise.complete.obs")
 return(myK)
 }
 
